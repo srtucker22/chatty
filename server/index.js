9747e322922ad730f65668a4be23d05dee46fb5b
Simon Tucker <srtucker22@gmail.com>
2017-05-25 14:31:42 -0500

    Step 2.7: Connect Resolvers to GraphQL Server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './data/schema';
 import { mocks } from './data/mocks';
+import { resolvers } from './data/resolvers';
 
 const PORT = 8080;
 
-const server = new ApolloServer({ typeDefs, mocks });
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  // mocks,
+});
 
 server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
