1231364e113a0266d69f874fe758393a2ddbe0bb
Simon Tucker <srtucker22@gmail.com>
2017-05-25 13:44:13 -0500

    Step 1.8: Add ApolloProvider to App

--- client/src/app.js
+++ client/src/app.js
@@ -58,11 +58,15 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <Text style={styles.welcome}>Welcome to React Native!</Text>
+            <Text style={styles.instructions}>To get started, edit App.js</Text>
+            <Text style={styles.instructions}>{instructions}</Text>
+          </View>
+        </Provider>
+      </ApolloProvider>
     );
   }
 }
