9ed5df27ed52ddefd988870ddc0b5e7ebcea5ac6
Simon Tucker <srtucker22@gmail.com>
2017-05-25 13:42:02 -0500

    Step 1.10: Add ApolloClient

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
@@ -11,6 +5,48 @@ import {
   Text,
   View,
 } from 'react-native';
+
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createHttpLink } from 'apollo-link-http';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
+import ReduxLink from 'apollo-link-redux';
+import { onError } from 'apollo-link-error';
+
+const URL = 'localhost:8080'; // set your comp's url here
+
+const store = createStore(
+  combineReducers({
+    apollo: apolloReducer,
+  }),
+  {}, // initial state
+  composeWithDevTools(),
+);
+
+const cache = new ReduxCache({ store });
+
+const reduxLink = new ReduxLink(store);
+
+const errorLink = onError((errors) => {
+  console.log(errors);
+});
+
+const httpLink = createHttpLink({ uri: `http://${URL}/graphql` });
+
+const link = ApolloLink.from([
+  reduxLink,
+  errorLink,
+  httpLink,
+]);
+
+export const client = new ApolloClient({
+  link,
+  cache,
+});
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
