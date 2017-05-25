11ac5344f3dabfabbc7b3a2d69889a0b70e7cc4a
Simon Tucker <srtucker22@gmail.com>
2017-05-25 14:28:38 -0500

    Step 2.6: Create Resolvers

---
+++ server/data/resolvers.js
@@ -0,0 +1,56 @@
+import GraphQLDate from 'graphql-date';
+
+import { Group, Message, User } from './connectors';
+
+export const Resolvers = {
+  Date: GraphQLDate,
+  Query: {
+    group(_, args) {
+      return Group.find({ where: args });
+    },
+    messages(_, args) {
+      return Message.findAll({
+        where: args,
+        order: [['createdAt', 'DESC']],
+      });
+    },
+    user(_, args) {
+      return User.findOne({ where: args });
+    },
+  },
+  Group: {
+    users(group) {
+      return group.getUsers();
+    },
+    messages(group) {
+      return Message.findAll({
+        where: { groupId: group.id },
+        order: [['createdAt', 'DESC']],
+      });
+    },
+  },
+  Message: {
+    to(message) {
+      return message.getGroup();
+    },
+    from(message) {
+      return message.getUser();
+    },
+  },
+  User: {
+    messages(user) {
+      return Message.findAll({
+        where: { userId: user.id },
+        order: [['createdAt', 'DESC']],
+      });
+    },
+    groups(user) {
+      return user.getGroups();
+    },
+    friends(user) {
+      return user.getFriends();
+    },
+  },
+};
+
+export default Resolvers;
