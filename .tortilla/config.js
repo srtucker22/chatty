82f4e403621b82f4e6ac176dafaeb12360407a80
Simon Tucker <srtucker22@gmail.com>
2017-05-25 12:51:10 -0500

    Chatty

---
+++ .tortilla/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  render: {
+    blacklist: /((package-lock.json)|\.(?:lock)$)/g,
+    transformations: {
+      'modified-medium': (Handlebars, view) => {
+        return view
+          .split(/```diff\n|\n```(?!diff)/).map((chunk, index) => {
+            if (index % 2 === 0) { return chunk; }
+
+            const content = Handlebars.escapeExpression(chunk)
+              // Add ellipses between new code
+              .replace(/^@.+$/m, '\n...\n')
+              // Remove removals
+              .replace(/\n\-.+/g, '')
+              // Bold additions
+              .replace(/^(\+.+)$/mg, '<b>$&</b>')
+              // Remove numbers
+              .replace(/\+?┊?([0-9]| ).*┊/mg, '');
+            // Wrap with <pre> tag
+            return `<pre>\n${content}\n</pre>`;
+          })
+          .join('');
+      }
+    }
+  }
+}
