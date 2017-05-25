f457122047c5c8b88306668635d936bc6d967e94
Simon Tucker <srtucker22@gmail.com>
2017-05-25 13:42:02 -0500

    Step 1.10: Add ApolloClient

--- client/src/app.js
+++ client/src/app.js
@@ -5,6 +5,26 @@ import {
   View,
 } from 'react-native';
 
+import { ApolloProvider } from 'react-apollo';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
+
+const networkInterface = createNetworkInterface({ uri: 'http://localhost:8080/graphql' });
+const client = new ApolloClient({
+  networkInterface,
+});
+
+const store = createStore(
+  combineReducers({
+    apollo: client.reducer(),
+  }),
+  {}, // initial state
+  composeWithDevTools(
+    applyMiddleware(client.middleware()),
+  ),
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
