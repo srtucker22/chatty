4c160f857fb9016eaaa49146daec896469b4b5ea
Simon Tucker <srtucker22@gmail.com>
2017-05-25 17:27:05 -0500

    Step 3.1: Create AppWithNavigationState

---
+++ client/src/navigation.js
@@ -0,0 +1,75 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { addNavigationHelpers, StackNavigator, TabNavigator } from 'react-navigation';
+import { Text, View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  tabText: {
+    color: '#777',
+    fontSize: 10,
+    justifyContent: 'center',
+  },
+  selected: {
+    color: 'blue',
+  },
+});
+
+const TestScreen = title => () => (
+  <View style={styles.container}>
+    <Text>
+      {title}
+    </Text>
+  </View>
+);
+
+// tabs in main screen
+const MainScreenNavigator = TabNavigator({
+  Chats: { screen: TestScreen('Chats') },
+  Settings: { screen: TestScreen('Settings') },
+});
+
+const AppNavigator = StackNavigator({
+  Main: { screen: MainScreenNavigator },
+});
+
+// reducer initialization code
+const firstAction = AppNavigator.router.getActionForPathAndParams('Main');
+const tempNavState = AppNavigator.router.getStateForAction(firstAction);
+const initialNavState = AppNavigator.router.getStateForAction(
+  tempNavState,
+);
+
+// reducer code
+export const navigationReducer = (state = initialNavState, action) => {
+  let nextState;
+  switch (action.type) {
+    default:
+      nextState = AppNavigator.router.getStateForAction(action, state);
+      break;
+  }
+
+  // Simply return the original `state` if `nextState` is null or undefined.
+  return nextState || state;
+};
+
+const AppWithNavigationState = ({ dispatch, nav }) => (
+  <AppNavigator navigation={addNavigationHelpers({ dispatch, state: nav })} />
+);
+
+AppWithNavigationState.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  nav: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+export default connect(mapStateToProps)(AppWithNavigationState);
