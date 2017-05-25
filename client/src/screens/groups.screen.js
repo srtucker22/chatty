a92df4b59adc23cc7b3986997b001fc8478f4fea
Simon Tucker <srtucker22@gmail.com>
2017-05-25 18:47:15 -0500

    Step 3.3: Create Groups Screen

---
+++ client/src/screens/groups.screen.js
@@ -0,0 +1,84 @@
+import { _ } from 'lodash';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  groupContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+    paddingHorizontal: 12,
+    paddingVertical: 8,
+  },
+  groupName: {
+    fontWeight: 'bold',
+    flex: 0.7,
+  },
+});
+
+// create fake data to populate our ListView
+const fakeData = () => _.times(100, i => ({
+  id: i,
+  name: `Group ${i}`,
+}));
+
+class Group extends Component {
+  render() {
+    const { id, name } = this.props.group;
+    return (
+      <TouchableHighlight
+        key={id}
+      >
+        <View style={styles.groupContainer}>
+          <Text style={styles.groupName}>{`${name}`}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+}
+
+Group.propTypes = {
+  group: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+  }),
+};
+
+class Groups extends Component {
+  static navigationOptions = {
+    title: 'Chats',
+  };
+
+  keyExtractor = item => item.id.toString();
+
+  renderItem = ({ item }) => <Group group={item} />;
+
+  render() {
+    // render list of groups for user
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={fakeData()}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItem}
+        />
+      </View>
+    );
+  }
+}
+
+export default Groups;
