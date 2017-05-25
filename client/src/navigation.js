766d0ca4e584f4ed572e08646e0d3e1b73cb91ff
Simon Tucker <srtucker22@gmail.com>
2017-05-25 17:27:05 -0500

    Step 3.1: Create AppWithNavigationState

---
+++ client/src/navigation.js
@@ -0,0 +1,88 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { NavigationActions, addNavigationHelpers, StackNavigator, TabNavigator } from 'react-navigation';
+import {
+  createReduxBoundAddListener,
+  createReactNavigationReduxMiddleware,
+} from 'react-navigation-redux-helpers';
+import { Text, View, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  tabText: {
+    color: '#777',
+    fontSize: 10,
+    justifyContent: 'center',
+  },
+  selected: {
+    color: 'blue',
+  },
+});
+
+const TestScreen = title => () => (
+  <View style={styles.container}>
+    <Text>
+      {title}
+    </Text>
+  </View>
+);
+
+// tabs in main screen
+const MainScreenNavigator = TabNavigator({
+  Chats: { screen: TestScreen('Chats') },
+  Settings: { screen: TestScreen('Settings') },
+}, {
+  initialRouteName: 'Chats',
+});
+
+const AppNavigator = StackNavigator({
+  Main: { screen: MainScreenNavigator },
+});
+
+// reducer initialization code
+const initialState=AppNavigator.router.getStateForAction(NavigationActions.reset({
+	index: 0,
+	actions: [
+	  NavigationActions.navigate({
+		  routeName: 'Main',
+	  }),
+	],
+}));
+
+export const navigationReducer = (state = initialState, action) => {
+  const nextState = AppNavigator.router.getStateForAction(action, state);
+
+  // Simply return the original `state` if `nextState` is null or undefined.
+  return nextState || state;
+};
+
+// Note: createReactNavigationReduxMiddleware must be run before createReduxBoundAddListener
+export const navigationMiddleware = createReactNavigationReduxMiddleware(
+  "root",
+  state => state.nav,
+);
+const addListener = createReduxBoundAddListener("root");
+
+class AppWithNavigationState extends Component {
+  render() {
+    return (
+      <AppNavigator navigation={addNavigationHelpers({
+        dispatch: this.props.dispatch,
+        state: this.props.nav,
+        addListener,
+      })} />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+export default connect(mapStateToProps)(AppWithNavigationState);
