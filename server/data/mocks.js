a997f800bd2a8f219d0824e96a3059495b81bd97
Simon Tucker <srtucker22@gmail.com>
2017-05-25 14:23:06 -0500

    Step 2.3: Update Mocks

--- server/data/mocks.js
+++ server/data/mocks.js
@@ -1,5 +1,29 @@
+import faker from 'faker';
+
 export const Mocks = {
+  Date: () => new Date(),
+  Int: () => parseInt(Math.random() * 100, 10),
   String: () => 'It works!',
+  Query: () => ({
+    user: (root, args) => ({
+      email: args.email,
+      messages: [{
+        from: {
+          email: args.email,
+        },
+      }],
+    }),
+  }),
+  User: () => ({
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+  }),
+  Group: () => ({
+    name: faker.lorem.words(Math.random() * 3),
+  }),
+  Message: () => ({
+    text: faker.lorem.sentences(Math.random() * 3),
+  }),
 };
 
 export default Mocks;
