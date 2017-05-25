b83c719b06f200f24a7296d72f94621720fe62fb
Simon Tucker <srtucker22@gmail.com>
2017-05-25 14:17:49 -0500

    Step 2.1: Update Schema

--- server/data/schema.js
+++ server/data/schema.js
@@ -1,10 +1,33 @@
-export const Schema = [
-  `type Query {
-    testString: String
+export const Schema = [`
+  # declare custom scalars
+  scalar Date
+
+  # a group chat entity
+  type Group {
+    id: Int! # unique id for the group
+    name: String # name of the group
+    users: [User]! # users in the group
+    messages: [Message] # messages sent to the group
   }
-  schema {
-    query: Query
-  }`,
-];
+
+  # a user -- keep type really simple for now
+  type User {
+    id: Int! # unique id for the user
+    email: String! # we will also require a unique email per user
+    username: String # this is the name we'll show other users
+    messages: [Message] # messages sent by user
+    groups: [Group] # groups the user belongs to
+    friends: [User] # user's friends/contacts
+  }
+
+  # a message sent from a user to a group
+  type Message {
+    id: Int! # unique id for message
+    to: Group! # group message was sent in
+    from: User! # user who sent the message
+    text: String! # message text
+    createdAt: Date! # when message was created
+  }
+`];
 
 export default Schema;
