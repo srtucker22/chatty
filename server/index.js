09a3f29e91a7e6a1ef4683462296998a75ebc3f2
Simon Tucker <srtucker22@gmail.com>
2017-05-25 14:23:06 -0500

    Step 2.3: Update Mocks

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './data/schema';
+import { mocks } from './data/mocks';
 
 const PORT = 8080;
 
-const server = new ApolloServer({ typeDefs, mocks: true });
+const server = new ApolloServer({ typeDefs, mocks });
 
 server.listen({ port: PORT }).then(({ url }) => console.log(`🚀 Server ready at ${url}`));
