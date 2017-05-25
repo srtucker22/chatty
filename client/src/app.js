2ef1642f3095074ee3526c40ad0fca92561d0e36
Simon Tucker <srtucker22@gmail.com>
2017-05-25 13:38:52 -0500

    Step 1.6: Move app code to /src

--- client/src/app.js
+++ client/src/app.js
@@ -16,8 +16,7 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
