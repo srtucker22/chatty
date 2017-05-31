ac433094fa94fad1fbbbeb82916b3b88ae54015a
Simon Tucker <srtucker22@gmail.com>
2017-05-31 18:58:27 -0500

    Step 5.5: Add fetchMore to groupQuery

--- client/src/screens/messages.screen.js
+++ client/src/screens/messages.screen.js
@@ -12,6 +12,8 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import randomColor from 'randomcolor';
 import { graphql, compose } from 'react-apollo';
+import update from 'immutability-helper';
+import { Buffer } from 'buffer';
 
 import Message from '../components/message.component';
 import MessageInput from '../components/message-input.component';
@@ -84,7 +86,6 @@ class Messages extends Component {
 
     this.state = {
       usernameColors,
-      refreshing: false,
     };
 
     this.renderItem = this.renderItem.bind(this);
@@ -123,15 +124,19 @@ class Messages extends Component {
     });
   }
 
-  keyExtractor = item => item.id;
+  keyExtractor = item => item.node.id;
 
-  renderItem = ({ item: message }) => (
-    <Message
-      color={this.state.usernameColors[message.from.username]}
-      isCurrentUser={message.from.id === 1} // for now until we implement auth
-      message={message}
-    />
-  )
+  renderItem = ({ item: edge }) => {
+    const message = edge.node;
+
+    return (
+      <Message
+        color={this.state.usernameColors[message.from.username]}
+        isCurrentUser={message.from.id === 1} // for now until we implement auth
+        message={message}
+      />
+    );
+  }
 
   render() {
     const { loading, group } = this.props;
@@ -156,7 +161,7 @@ class Messages extends Component {
         <FlatList
           ref={(ref) => { this.flatList = ref; }}
           inverted
-          data={group.messages}
+          data={group.messages.edges}
           keyExtractor={this.keyExtractor}
           renderItem={this.renderItem}
           ListEmptyComponent={<View />}
@@ -179,20 +184,56 @@ Messages.propTypes = {
     }),
   }),
   group: PropTypes.shape({
-    messages: PropTypes.array,
+    messages: PropTypes.shape({
+      edges: PropTypes.arrayOf(PropTypes.shape({
+        cursor: PropTypes.string,
+        node: PropTypes.object,
+      })),
+      pageInfo: PropTypes.shape({
+        hasNextPage: PropTypes.bool,
+        hasPreviousPage: PropTypes.bool,
+      }),
+    }),
     users: PropTypes.array,
   }),
   loading: PropTypes.bool,
+  loadMoreEntries: PropTypes.func,
 };
 
+const ITEMS_PER_PAGE = 10;
 const groupQuery = graphql(GROUP_QUERY, {
   options: ownProps => ({
     variables: {
       groupId: ownProps.navigation.state.params.groupId,
+      first: ITEMS_PER_PAGE,
     },
   }),
-  props: ({ data: { loading, group } }) => ({
-    loading, group,
+  props: ({ data: { fetchMore, loading, group } }) => ({
+    loading,
+    group,
+    loadMoreEntries() {
+      return fetchMore({
+        // query: ... (you can specify a different query.
+        // GROUP_QUERY is used by default)
+        variables: {
+          // load more queries starting from the cursor of the last (oldest) message
+          after: group.messages.edges[group.messages.edges.length - 1].cursor,
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          // we will make an extra call to check if no more entries
+          if (!fetchMoreResult) { return previousResult; }
+          // push results (older messages) to end of messages list
+          return update(previousResult, {
+            group: {
+              messages: {
+                edges: { $push: fetchMoreResult.group.messages.edges },
+                pageInfo: { $set: fetchMoreResult.group.messages.pageInfo },
+              },
+            },
+          });
+        },
+      });
+    },
   }),
 });
 
@@ -225,17 +266,23 @@ const createMessageMutation = graphql(CREATE_MESSAGE_MUTATION, {
             query: GROUP_QUERY,
             variables: {
               groupId,
+              first: ITEMS_PER_PAGE,
             },
           });
 
           // Add our message from the mutation to the end.
-          groupData.group.messages.unshift(createMessage);
+          groupData.group.messages.edges.unshift({
+            __typename: 'MessageEdge',
+            node: createMessage,
+            cursor: Buffer.from(createMessage.id.toString()).toString('base64'),
+          });
 
           // Write our data back to the cache.
           store.writeQuery({
             query: GROUP_QUERY,
             variables: {
               groupId,
+              first: ITEMS_PER_PAGE,
             },
             data: groupData,
           });
