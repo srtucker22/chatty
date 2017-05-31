b0825796d434271a40f89a4c0d0576a78713c8c0
Simon Tucker <srtucker22@gmail.com>
2017-05-31 18:54:25 -0500

    Step 5.4: Update Group Query with Relay Cursor Connections

--- client/src/graphql/group.query.js
+++ client/src/graphql/group.query.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 import MESSAGE_FRAGMENT from './message.fragment';
 
 const GROUP_QUERY = gql`
-  query group($groupId: Int!) {
+  query group($groupId: Int!, $first: Int, $after: String, $last: Int, $before: String) {
     group(id: $groupId) {
       id
       name
@@ -11,8 +11,17 @@ const GROUP_QUERY = gql`
         id
         username
       }
-      messages {
-        ... MessageFragment
+      messages(first: $first, after: $after, last: $last, before: $before) {
+        edges {
+          cursor
+          node {
+            ... MessageFragment
+          }
+        }
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+        }
       }
     }
   }
