06a15e7b2cce0ac9eb3cde5207e21f8c3c32f206
Simon Tucker <srtucker22@gmail.com>
2017-06-01 18:27:13 -0500

    Step 7.32: Update Messages Screen and createMessage with auth

--- client/src/graphql/create-message.mutation.js
+++ client/src/graphql/create-message.mutation.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag';
 import MESSAGE_FRAGMENT from './message.fragment';
 
 const CREATE_MESSAGE_MUTATION = gql`
-  mutation createMessage($text: String!, $userId: Int!, $groupId: Int!) {
-    createMessage(text: $text, userId: $userId, groupId: $groupId) {
+  mutation createMessage($text: String!, $groupId: Int!) {
+    createMessage(text: $text, groupId: $groupId) {
       ... MessageFragment
     }
   }
