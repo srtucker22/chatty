8fef92d1e13985087f6c9bc68e8afb9c2a71f7e2
Simon Tucker <srtucker22@gmail.com>
2017-05-31 19:54:04 -0500

    Step 6.6: Create SubscriptionServer

--- server/index.js
+++ server/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import bodyParser from 'body-parser';
 import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
+import { execute, subscribe } from 'graphql';
 
 import { executableSchema } from './data/schema';
 
 const GRAPHQL_PORT = 8080;
+const GRAPHQL_PATH = '/graphql';
+const SUBSCRIPTIONS_PATH = '/subscriptions';
+
 const app = express();
 
 // `context` must be an object and can't be undefined when using connectors
@@ -15,9 +20,23 @@ app.use('/graphql', bodyParser.json(), graphqlExpress({
 }));
 
 app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
+  endpointURL: GRAPHQL_PATH,
+  subscriptionsEndpoint: `ws://localhost:${GRAPHQL_PORT}${SUBSCRIPTIONS_PATH}`,
 }));
 
 const graphQLServer = createServer(app);
 
-graphQLServer.listen(GRAPHQL_PORT, () => console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`));
+graphQLServer.listen(GRAPHQL_PORT, () => {
+  console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}${GRAPHQL_PATH}`);
+  console.log(`GraphQL Subscriptions are now running on ws://localhost:${GRAPHQL_PORT}${SUBSCRIPTIONS_PATH}`);
+});
+
+// eslint-disable-next-line no-unused-vars
+const subscriptionServer = SubscriptionServer.create({
+  schema: executableSchema,
+  execute,
+  subscribe,
+}, {
+  server: graphQLServer,
+  path: SUBSCRIPTIONS_PATH,
+});
