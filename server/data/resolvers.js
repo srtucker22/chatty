f0760af8bc19c862710cc843dabe472fec4d1260
Simon Tucker <srtucker22@gmail.com>
2017-05-31 19:57:41 -0500

    Step 6.6: Add groupAdded to Resolvers

--- server/data/resolvers.js
+++ server/data/resolvers.js
@@ -5,6 +5,7 @@ import { Group, Message, User } from './connectors';
 import { pubsub } from '../subscriptions';
 
 const MESSAGE_ADDED_TOPIC = 'messageAdded';
+const GROUP_ADDED_TOPIC = 'groupAdded';
 
 export const resolvers = {
   Date: GraphQLDate,
@@ -51,8 +52,13 @@ export const resolvers = {
             users: [user, ...friends],
           })
             .then(group => group.addUsers([user, ...friends])
-              .then(() => group),
-            ),
+              .then((res) => {
+                // append the user list to the group object
+                // to pass to pubsub so we can check members
+                group.users = [user, ...friends];
+                pubsub.publish(GROUP_ADDED_TOPIC, { [GROUP_ADDED_TOPIC]: group });
+                return group;
+              })),
           ),
         );
     },
@@ -94,6 +100,9 @@ export const resolvers = {
           );
         },
       ),
+    },
+    groupAdded: {
+      subscribe: () => pubsub.asyncIterator(GROUP_ADDED_TOPIC),
     },
   },
   Group: {
