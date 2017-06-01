bd6a4b326107215d38e007b92b00a97d062bb175
Simon Tucker <srtucker22@gmail.com>
2017-06-01 15:18:16 -0500

    Step 7.21: Handle login/logout in auth reducer

--- client/src/reducers/auth.reducer.js
+++ client/src/reducers/auth.reducer.js
@@ -1,6 +1,8 @@
 import { REHYDRATE } from 'redux-persist';
 import Immutable from 'seamless-immutable';
 
+import { LOGOUT, SET_CURRENT_USER } from '../constants/constants';
+
 const initialState = Immutable({
   loading: true,
 });
@@ -9,8 +11,13 @@ const auth = (state = initialState, action) => {
   switch (action.type) {
     case REHYDRATE:
       // convert persisted data to Immutable and confirm rehydration
-      return Immutable(action.payload.auth || state)
+      const { payload = {} } = action;
+      return Immutable(payload.auth || state)
         .set('loading', false);
+    case SET_CURRENT_USER:
+      return state.merge(action.user);
+    case LOGOUT:
+      return Immutable({ loading: false });
     default:
       return state;
   }
