ddbf06bc1ac2f9d0e0259a24e4128cc116095172
Simon Tucker <srtucker22@gmail.com>
2017-06-01 02:14:56 -0500

    Step 7.3: Update Schema with auth

--- server/data/schema.js
+++ server/data/schema.js
@@ -38,6 +38,7 @@ export const Schema = [`
     messages: [Message] # messages sent by user
     groups: [Group] # groups the user belongs to
     friends: [User] # user's friends/contacts
+    jwt: String # json web token for access
   }
 
   # a message sent from a user to a group
@@ -64,19 +65,19 @@ export const Schema = [`
 
   type Mutation {
     # send a message to a group
-    createMessage(
-      text: String!, userId: Int!, groupId: Int!
-    ): Message
-    createGroup(name: String!, userIds: [Int], userId: Int!): Group
+    createMessage(text: String!, groupId: Int!): Message
+    createGroup(name: String!, userIds: [Int]): Group
     deleteGroup(id: Int!): Group
-    leaveGroup(id: Int!, userId: Int!): Group # let user leave group
+    leaveGroup(id: Int!): Group # let user leave group
     updateGroup(id: Int!, name: String): Group
+    login(email: String!, password: String!): User
+    signup(email: String!, password: String!, username: String): User
   }
 
   type Subscription {
     # Subscription fires on every message added
     # for any of the groups with one of these groupIds
-    messageAdded(userId: Int, groupIds: [Int]): Message
+    messageAdded(groupIds: [Int]): Message
     groupAdded(userId: Int): Group
   }
   
