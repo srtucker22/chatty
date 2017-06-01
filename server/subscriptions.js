1cbaab9752997aa00f6b3232295d4215d2eedfe9
Simon Tucker <srtucker22@gmail.com>
2017-06-01 14:59:01 -0500

    Step 7.13: Create subscriptionLogic

--- server/subscriptions.js
+++ server/subscriptions.js
@@ -1,5 +1,24 @@
+import { $$asyncIterator } from 'iterall';
 import { PubSub } from 'apollo-server';
 
 export const pubsub = new PubSub();
+
+pubsub.asyncAuthIterator = (messages, authPromise) => {
+  const asyncIterator = pubsub.asyncIterator(messages);
+  return {
+    next() {
+      return authPromise.then(() => asyncIterator.next());
+    },
+    return() {
+      return authPromise.then(() => asyncIterator.return());
+    },
+    throw(error) {
+      return asyncIterator.throw(error);
+    },
+    [$$asyncIterator]() {
+      return asyncIterator;
+    },
+  };
+};
 
 export default pubsub;
