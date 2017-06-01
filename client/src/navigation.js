a2e2cc74dd4fcd69ea4e802e7a731c9340e3d062
Simon Tucker <srtucker22@gmail.com>
2017-05-31 22:40:42 -0500

    Step 6.12: Apply subscribeToMore to AppWithNavigationState

--- client/src/navigation.js
+++ client/src/navigation.js
@@ -7,12 +7,20 @@ import {
 } from 'react-navigation-redux-helpers';
 import { Text, View, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
+import { graphql, compose } from 'react-apollo';
+import update from 'immutability-helper';
+import { map } from 'lodash';
+import { Buffer } from 'buffer';
 
 import Groups from './screens/groups.screen';
 import Messages from './screens/messages.screen';
 import FinalizeGroup from './screens/finalize-group.screen';
 import GroupDetails from './screens/group-details.screen';
 import NewGroup from './screens/new-group.screen';
+
+import { USER_QUERY } from './graphql/user.query';
+import MESSAGE_ADDED_SUBSCRIPTION from './graphql/message-added.subscription';
+import GROUP_ADDED_SUBSCRIPTION from './graphql/group-added.subscription';
 
 const styles = StyleSheet.create({
   container: {
@@ -82,6 +90,35 @@ export const navigationMiddleware = createReactNavigationReduxMiddleware(
 const addListener = createReduxBoundAddListener("root");
 
 class AppWithNavigationState extends Component {
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.user) {
+      if (this.groupSubscription) {
+        this.groupSubscription();
+      }
+
+      if (this.messagesSubscription) {
+        this.messagesSubscription();
+      }
+    }
+
+    if (nextProps.user &&
+      (!this.props.user || nextProps.user.groups.length !== this.props.user.groups.length)) {
+      // unsubscribe from old
+
+      if (typeof this.messagesSubscription === 'function') {
+        this.messagesSubscription();
+      }
+      // subscribe to new
+      if (nextProps.user.groups.length) {
+        this.messagesSubscription = nextProps.subscribeToMessages();
+      }
+    }
+
+    if (!this.groupSubscription && nextProps.user) {
+      this.groupSubscription = nextProps.subscribeToGroups();
+    }
+  }
+
   render() {
     return (
       <AppNavigator navigation={addNavigationHelpers({
@@ -93,8 +130,84 @@ class AppWithNavigationState extends Component {
   }
 }
 
+AppWithNavigationState.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  nav: PropTypes.object.isRequired,
+  subscribeToGroups: PropTypes.func,
+  subscribeToMessages: PropTypes.func,
+  user: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    email: PropTypes.string.isRequired,
+    groups: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+      }),
+    ),
+  }),
+};
+
 const mapStateToProps = state => ({
   nav: state.nav,
 });
 
-export default connect(mapStateToProps)(AppWithNavigationState);
+const userQuery = graphql(USER_QUERY, {
+  options: () => ({ variables: { id: 1 } }), // fake the user for now
+  props: ({ data: { loading, user, subscribeToMore } }) => ({
+    loading,
+    user,
+    subscribeToMessages() {
+      return subscribeToMore({
+        document: MESSAGE_ADDED_SUBSCRIPTION,
+        variables: {
+          userId: 1, // fake the user for now
+          groupIds: map(user.groups, 'id'),
+        },
+        updateQuery: (previousResult, { subscriptionData }) => {
+          const previousGroups = previousResult.user.groups;
+          const newMessage = subscriptionData.data.messageAdded;
+
+          const groupIndex = map(previousGroups, 'id').indexOf(newMessage.to.id);
+
+          return update(previousResult, {
+            user: {
+              groups: {
+                [groupIndex]: {
+                  messages: {
+                    edges: {
+                      $set: [{
+                        __typename: 'MessageEdge',
+                        node: newMessage,
+                        cursor: Buffer.from(newMessage.id.toString()).toString('base64'),
+                      }],
+                    },
+                  },
+                },
+              },
+            },
+          });
+        },
+      });
+    },
+    subscribeToGroups() {
+      return subscribeToMore({
+        document: GROUP_ADDED_SUBSCRIPTION,
+        variables: { userId: user.id },
+        updateQuery: (previousResult, { subscriptionData }) => {
+          const newGroup = subscriptionData.data.groupAdded;
+
+          return update(previousResult, {
+            user: {
+              groups: { $push: [newGroup] },
+            },
+          });
+        },
+      });
+    },
+  }),
+});
+
+export default compose(
+  connect(mapStateToProps),
+  userQuery,
+)(AppWithNavigationState);
