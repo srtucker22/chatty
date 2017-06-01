d49c1547d3b143ba825bdb8e069c05b2381a8792
Simon Tucker <srtucker22@gmail.com>
2017-05-31 19:23:42 -0500

    Step 5.8: Modify Group component to include latest message

--- client/src/screens/groups.screen.js
+++ client/src/screens/groups.screen.js
@@ -4,12 +4,15 @@ import {
   FlatList,
   ActivityIndicator,
   Button,
+  Image,
   StyleSheet,
   Text,
   TouchableHighlight,
   View,
 } from 'react-native';
 import { graphql } from 'react-apollo';
+import moment from 'moment';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { USER_QUERY } from '../graphql/user.query';
 
@@ -36,6 +39,31 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     flex: 0.7,
   },
+  groupTextContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    paddingLeft: 6,
+  },
+  groupText: {
+    color: '#8c8c8c',
+  },
+  groupImage: {
+    width: 54,
+    height: 54,
+    borderRadius: 27,
+  },
+  groupTitleContainer: {
+    flexDirection: 'row',
+  },
+  groupLastUpdated: {
+    flex: 0.3,
+    color: '#8c8c8c',
+    fontSize: 11,
+    textAlign: 'right',
+  },
+  groupUsername: {
+    paddingVertical: 4,
+  },
   header: {
     alignItems: 'flex-end',
     padding: 6,
@@ -46,6 +74,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 12,
   },
+});
+
+// format createdAt with moment
+const formatCreatedAt = createdAt => moment(createdAt).calendar(null, {
+  sameDay: '[Today]',
+  nextDay: '[Tomorrow]',
+  nextWeek: 'dddd',
+  lastDay: '[Yesterday]',
+  lastWeek: 'dddd',
+  sameElse: 'DD/MM/YYYY',
 });
 
 const Header = ({ onPress }) => (
@@ -65,14 +103,40 @@ class Group extends Component {
   }
 
   render() {
-    const { id, name } = this.props.group;
+    const { id, name, messages } = this.props.group;
     return (
       <TouchableHighlight
         key={id}
         onPress={this.goToMessages}
       >
         <View style={styles.groupContainer}>
-          <Text style={styles.groupName}>{`${name}`}</Text>
+          <Image
+            style={styles.groupImage}
+            source={{
+              uri: 'https://reactjs.org/logo-og.png',
+            }}
+          />
+          <View style={styles.groupTextContainer}>
+            <View style={styles.groupTitleContainer}>
+              <Text style={styles.groupName}>{`${name}`}</Text>
+              <Text style={styles.groupLastUpdated}>
+                {messages.edges.length ?
+                  formatCreatedAt(messages.edges[0].node.createdAt) : ''}
+              </Text>
+            </View>
+            <Text style={styles.groupUsername}>
+              {messages.edges.length ?
+                `${messages.edges[0].node.from.username}:` : ''}
+            </Text>
+            <Text style={styles.groupText} numberOfLines={1}>
+              {messages.edges.length ? messages.edges[0].node.text : ''}
+            </Text>
+          </View>
+          <Icon
+            name="angle-right"
+            size={24}
+            color={'#8c8c8c'}
+          />
         </View>
       </TouchableHighlight>
     );
@@ -84,6 +148,12 @@ Group.propTypes = {
   group: PropTypes.shape({
     id: PropTypes.number,
     name: PropTypes.string,
+    messages: PropTypes.shape({
+      edges: PropTypes.arrayOf(PropTypes.shape({
+        cursor: PropTypes.string,
+        node: PropTypes.object,
+      })),
+    }),
   }),
 };
 
