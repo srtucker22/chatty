2be468c80e559b558e77813c3d737c9cf8f3da05
Simon Tucker <srtucker22@gmail.com>
2017-06-01 02:28:48 -0500

    Step 7.8: Create logic for all Resolvers

--- server/data/logic.js
+++ server/data/logic.js
@@ -1,5 +1,5 @@
 import { ApolloError, AuthenticationError, ForbiddenError } from 'apollo-server';
-import { Message } from './connectors';
+import { Group, Message, User } from './connectors';
 
 // reusable function to check for a user with context
 function getAuthenticatedUser(ctx) {
@@ -12,6 +12,12 @@ function getAuthenticatedUser(ctx) {
 }
 
 export const messageLogic = {
+  from(message) {
+    return message.getUser({ attributes: ['id', 'username'] });
+  },
+  to(message) {
+    return message.getGroup({ attributes: ['id', 'name'] });
+  },
   createMessage(_, { text, groupId }, ctx) {
     return getAuthenticatedUser(ctx)
       .then(user => user.getGroups({ where: { id: groupId }, attributes: ['id'] })
@@ -25,5 +31,196 @@ export const messageLogic = {
           }
           throw new ForbiddenError('Unauthorized');
         }));
+  },
+};
+
+export const groupLogic = {
+  users(group) {
+    return group.getUsers({ attributes: ['id', 'username'] });
+  },
+  messages(group, { first, last, before, after }) {
+    // base query -- get messages from the right group
+    const where = { groupId: group.id };
+
+    // because we return messages from newest -> oldest
+    // before actually means newer (date > cursor)
+    // after actually means older (date < cursor)
+
+    if (before) {
+      // convert base-64 to utf8 iso date and use in Date constructor
+      where.id = { $gt: Buffer.from(before, 'base64').toString() };
+    }
+
+    if (after) {
+      where.id = { $lt: Buffer.from(after, 'base64').toString() };
+    }
+
+    return Message.findAll({
+      where,
+      order: [['id', 'DESC']],
+      limit: first || last,
+    }).then((messages) => {
+      const edges = messages.map(message => ({
+        cursor: Buffer.from(message.id.toString()).toString('base64'), // convert createdAt to cursor
+        node: message, // the node is the message itself
+      }));
+
+      return {
+        edges,
+        pageInfo: {
+          hasNextPage() {
+            if (messages.length < (last || first)) {
+              return Promise.resolve(false);
+            }
+
+            return Message.findOne({
+              where: {
+                groupId: group.id,
+                id: {
+                  [before ? '$gt' : '$lt']: messages[messages.length - 1].id,
+                },
+              },
+              order: [['id', 'DESC']],
+            }).then(message => !!message);
+          },
+          hasPreviousPage() {
+            return Message.findOne({
+              where: {
+                groupId: group.id,
+                id: where.id,
+              },
+              order: [['id']],
+            }).then(message => !!message);
+          },
+        },
+      };
+    });
+  },
+  query(_, { id }, ctx) {
+    return getAuthenticatedUser(ctx).then(user => Group.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        where: { id: user.id },
+      }],
+    }));
+  },
+  createGroup(_, { name, userIds }, ctx) {
+    return getAuthenticatedUser(ctx)
+      .then(user => user.getFriends({ where: { id: { $in: userIds } } })
+        .then((friends) => { // eslint-disable-line arrow-body-style
+          return Group.create({
+            name,
+          }).then((group) => { // eslint-disable-line arrow-body-style
+            return group.addUsers([user, ...friends]).then(() => {
+              group.users = [user, ...friends];
+              return group;
+            });
+          });
+        }));
+  },
+  deleteGroup(_, { id }, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => { // eslint-disable-line arrow-body-style
+      return Group.findOne({
+        where: { id },
+        include: [{
+          model: User,
+          where: { id: user.id },
+        }],
+      }).then(group => group.getUsers()
+        .then(users => group.removeUsers(users))
+        .then(() => Message.destroy({ where: { groupId: group.id } }))
+        .then(() => group.destroy()));
+    });
+  },
+  leaveGroup(_, { id }, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => {
+      return Group.findOne({
+        where: { id },
+        include: [{
+          model: User,
+          where: { id: user.id },
+        }],
+      }).then((group) => {
+        if (!group) {
+          throw new ApolloError('No group found', 404);
+        }
+
+        return group.removeUser(user.id)
+          .then(() => group.getUsers())
+          .then((users) => {
+            // if the last user is leaving, remove the group
+            if (!users.length) {
+              group.destroy();
+            }
+            return { id };
+          });
+      });
+    });
+  },
+  updateGroup(_, { id, name }, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => { // eslint-disable-line arrow-body-style
+      return Group.findOne({
+        where: { id },
+        include: [{
+          model: User,
+          where: { id: user.id },
+        }],
+      }).then(group => group.update({ name }));
+    });
+  },
+};
+
+export const userLogic = {
+  email(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id === user.id) {
+        return currentUser.email;
+      }
+
+      throw new ForbiddenError('Unauthorized');
+    });
+  },
+  friends(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id !== user.id) {
+        throw new ForbiddenError('Unauthorized');
+      }
+
+      return user.getFriends({ attributes: ['id', 'username'] });
+    });
+  },
+  groups(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id !== user.id) {
+        throw new ForbiddenError('Unauthorized');
+      }
+
+      return user.getGroups();
+    });
+  },
+  jwt(user) {
+    return Promise.resolve(user.jwt);
+  },
+  messages(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id !== user.id) {
+        throw new ForbiddenError('Unauthorized');
+      }
+
+      return Message.findAll({
+        where: { userId: user.id },
+        order: [['createdAt', 'DESC']],
+      });
+    });
+  },
+  query(_, args, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => {
+      if (user.id === args.id || user.email === args.email) {
+        return user;
+      }
+
+      throw new ForbiddenError('Unauthorized');
+    });
   },
 };
