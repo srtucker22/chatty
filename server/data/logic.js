ba87b217279c06a509a4e38518150a8bdadfe07d
Simon Tucker <srtucker22@gmail.com>
2017-06-01 02:28:48 -0500

    Step 7.8: Create logic for all Resolvers

--- server/data/logic.js
+++ server/data/logic.js
@@ -1,4 +1,4 @@
-import { Message } from './connectors';
+import { Group, Message, User } from './connectors';
 
 // reusable function to check for a user with context
 function getAuthenticatedUser(ctx) {
@@ -11,6 +11,12 @@ function getAuthenticatedUser(ctx) {
 }
 
 export const messageLogic = {
+  from(message) {
+    return message.getUser({ attributes: ['id', 'username'] });
+  },
+  to(message) {
+    return message.getGroup({ attributes: ['id', 'name'] });
+  },
   createMessage(_, { text, groupId }, ctx) {
     return getAuthenticatedUser(ctx)
       .then(user => user.getGroups({ where: { id: groupId }, attributes: ['id'] })
@@ -24,5 +30,143 @@ export const messageLogic = {
         }
         return Promise.reject('Unauthorized');
       }));
+  },
+};
+
+export const groupLogic = {
+  users(group) {
+    return group.getUsers({ attributes: ['id', 'username'] });
+  },
+  messages(group, args) {
+    return Message.findAll({
+      where: { groupId: group.id },
+      order: [['createdAt', 'DESC']],
+      limit: args.limit,
+      offset: args.offset,
+    });
+  },
+  query(_, { id }, ctx) {
+    return getAuthenticatedUser(ctx).then(user => Group.findOne({
+      where: { id },
+      include: [{
+        model: User,
+        where: { id: user.id },
+      }],
+    }));
+  },
+  createGroup(_, { name, userIds }, ctx) {
+    return getAuthenticatedUser(ctx)
+      .then(user => user.getFriends({ where: { id: { $in: userIds } } })
+      .then((friends) => {  // eslint-disable-line arrow-body-style
+        return Group.create({
+          name,
+        }).then((group) => {  // eslint-disable-line arrow-body-style
+          return group.addUsers([user, ...friends]).then(() => {
+            group.users = [user, ...friends];
+            return group;
+          });
+        });
+      }));
+  },
+  deleteGroup(_, { id }, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => { // eslint-disable-line arrow-body-style
+      return Group.findOne({
+        where: { id },
+        include: [{
+          model: User,
+          where: { id: user.id },
+        }],
+      }).then(group => group.getUsers()
+        .then(users => group.removeUsers(users))
+        .then(() => Message.destroy({ where: { groupId: group.id } }))
+        .then(() => group.destroy()));
+    });
+  },
+  leaveGroup(_, { id }, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => {
+      if (!user) {
+        return Promise.reject('Unauthorized');
+      }
+
+      return Group.findOne({
+        where: { id },
+        include: [{
+          model: User,
+          where: { id: user.id },
+        }],
+      }).then((group) => {
+        if (!group) {
+          Promise.reject('No group found');
+        }
+
+        group.removeUser(user.id);
+        return Promise.resolve({ id });
+      });
+    });
+  },
+  updateGroup(_, { id, name }, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => {  // eslint-disable-line arrow-body-style
+      return Group.findOne({
+        where: { id },
+        include: [{
+          model: User,
+          where: { id: user.id },
+        }],
+      }).then(group => group.update({ name }));
+    });
+  },
+};
+
+export const userLogic = {
+  email(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id === user.id) {
+        return currentUser.email;
+      }
+
+      return Promise.reject('Unauthorized');
+    });
+  },
+  friends(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id !== user.id) {
+        return Promise.reject('Unauthorized');
+      }
+
+      return user.getFriends({ attributes: ['id', 'username'] });
+    });
+  },
+  groups(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id !== user.id) {
+        return Promise.reject('Unauthorized');
+      }
+
+      return user.getGroups();
+    });
+  },
+  jwt(user) {
+    return Promise.resolve(user.jwt);
+  },
+  messages(user, args, ctx) {
+    return getAuthenticatedUser(ctx).then((currentUser) => {
+      if (currentUser.id !== user.id) {
+        return Promise.reject('Unauthorized');
+      }
+
+      return Message.findAll({
+        where: { userId: user.id },
+        order: [['createdAt', 'DESC']],
+      });
+    });
+  },
+  query(_, args, ctx) {
+    return getAuthenticatedUser(ctx).then((user) => {
+      if (user.id === args.id || user.email === args.email) {
+        return user;
+      }
+
+      return Promise.reject('Unauthorized');
+    });
   },
 };
