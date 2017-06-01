bd86088e0d245d09319b3337abff7fa46773ddc4
Simon Tucker <srtucker22@gmail.com>
2017-06-01 15:01:09 -0500

    Step 7.16: Add Signin to navigation and skip queries

--- client/src/navigation.js
+++ client/src/navigation.js
@@ -13,6 +13,7 @@ import Messages from './screens/messages.screen';
 import FinalizeGroup from './screens/finalize-group.screen';
 import GroupDetails from './screens/group-details.screen';
 import NewGroup from './screens/new-group.screen';
+import Signin from './screens/signin.screen';
 
 import { USER_QUERY } from './graphql/user.query';
 import MESSAGE_ADDED_SUBSCRIPTION from './graphql/message-added.subscription';
@@ -53,6 +54,7 @@ const MainScreenNavigator = TabNavigator({
 
 const AppNavigator = StackNavigator({
   Main: { screen: MainScreenNavigator },
+  Signin: { screen: Signin },
   Messages: { screen: Messages },
   GroupDetails: { screen: GroupDetails },
   NewGroup: { screen: NewGroup },
@@ -149,6 +151,7 @@ const mapStateToProps = state => ({
 });
 
 const userQuery = graphql(USER_QUERY, {
+  skip: ownProps => true, // fake it -- we'll use ownProps with auth
   options: () => ({ variables: { id: 1 } }), // fake the user for now
   props: ({ data: { loading, user, refetch, subscribeToMore } }) => ({
     loading,
