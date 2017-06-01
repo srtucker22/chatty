2035793231c2aea369845f3e73b322d90b674117
Simon Tucker <srtucker22@gmail.com>
2017-05-31 19:23:09 -0500

    Step 5.7: Add most recent message to each Group in USER_QUERY

--- client/src/graphql/create-group.mutation.js
+++ client/src/graphql/create-group.mutation.js
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag';
 
+import MESSAGE_FRAGMENT from './message.fragment';
+
 const CREATE_GROUP_MUTATION = gql`
   mutation createGroup($name: String!, $userIds: [Int!], $userId: Int!) {
     createGroup(name: $name, userIds: $userIds, userId: $userId) {
@@ -8,8 +10,17 @@ const CREATE_GROUP_MUTATION = gql`
       users {
         id
       }
+      messages(first: 1) { # we don't need to use variables
+        edges {
+          cursor
+          node {
+            ... MessageFragment
+          }
+        }
+      }
     }
   }
+  ${MESSAGE_FRAGMENT}
 `;
 
 export default CREATE_GROUP_MUTATION;
