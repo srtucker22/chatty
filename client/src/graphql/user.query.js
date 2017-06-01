fb301e8234ac3375790aabb78167dd9756bdd2e0
Simon Tucker <srtucker22@gmail.com>
2017-05-31 19:23:09 -0500

    Step 5.7: Add most recent message to each Group in USER_QUERY

--- client/src/graphql/user.query.js
+++ client/src/graphql/user.query.js
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag';
 
+import MESSAGE_FRAGMENT from './message.fragment';
+
 // get the user and all user's groups
 export const USER_QUERY = gql`
   query user($id: Int) {
@@ -10,6 +12,14 @@ export const USER_QUERY = gql`
       groups {
         id
         name
+        messages(first: 1) { # we don't need to use variables
+          edges {
+            cursor
+            node {
+              ... MessageFragment
+            }
+          }
+        }
       }
       friends {
         id
@@ -17,6 +27,7 @@ export const USER_QUERY = gql`
       }
     }
   }
+  ${MESSAGE_FRAGMENT}
 `;
 
 export default USER_QUERY;
