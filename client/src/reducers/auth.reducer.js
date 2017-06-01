6f84518720341263b77777449549fcbb70ccb65d
Simon Tucker <srtucker22@gmail.com>
2017-06-01 15:14:31 -0500

    Step 7.20: Handle rehydration in auth reducer

--- client/src/reducers/auth.reducer.js
+++ client/src/reducers/auth.reducer.js
@@ -1,3 +1,4 @@
+import { REHYDRATE } from 'redux-persist/constants';
 import Immutable from 'seamless-immutable';
 
 const initialState = Immutable({
@@ -6,6 +7,10 @@ const initialState = Immutable({
 
 const auth = (state = initialState, action) => {
   switch (action.type) {
+    case REHYDRATE:
+      // convert persisted data to Immutable and confirm rehydration
+      return Immutable(action.payload.auth || state)
+        .set('loading', false);
     default:
       return state;
   }
