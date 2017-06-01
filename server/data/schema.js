38a60456bb7cd8596b8c7be3642ad1e0469d1793
Simon Tucker <srtucker22@gmail.com>
2017-05-31 19:55:27 -0500

    Step 6.7: Add groupAdded to Schema

--- server/data/schema.js
+++ server/data/schema.js
@@ -77,6 +77,7 @@ export const Schema = [`
     # Subscription fires on every message added
     # for any of the groups with one of these groupIds
     messageAdded(userId: Int, groupIds: [Int]): Message
+    groupAdded(userId: Int): Group
   }
   
   schema {
