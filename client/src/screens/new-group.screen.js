566312443ec8521e3db0c6000b61879fb4447938
Simon Tucker <srtucker22@gmail.com>
2017-06-01 22:04:35 -0500

    Step 7.32: Update Groups flow with auth

--- client/src/screens/new-group.screen.js
+++ client/src/screens/new-group.screen.js
@@ -13,6 +13,7 @@ import { graphql, compose } from 'react-apollo';
 import AlphabetListView from 'react-native-alpha-listview';
 import update from 'immutability-helper';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { connect } from 'react-redux';
 
 import SelectedUserList from '../components/selected-user-list.component';
 import USER_QUERY from '../graphql/user.query';
@@ -309,12 +310,17 @@ NewGroup.propTypes = {
 };
 
 const userQuery = graphql(USER_QUERY, {
-  options: (ownProps) => ({ variables: { id: 1 } }), // fake for now
+  options: ownProps => ({ variables: { id: ownProps.auth.id } }),
   props: ({ data: { loading, user } }) => ({
     loading, user,
   }),
 });
 
+const mapStateToProps = ({ auth }) => ({
+  auth,
+});
+
 export default compose(
+  connect(mapStateToProps),
   userQuery,
 )(NewGroup);
