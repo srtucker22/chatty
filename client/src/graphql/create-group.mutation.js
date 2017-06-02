ccd6d19e25c1c3dc17640b2dcaf537e33896c263
Simon Tucker <srtucker22@gmail.com>
2017-06-01 22:04:35 -0500

    Step 7.32: Update Groups flow with auth

--- client/src/graphql/create-group.mutation.js
+++ client/src/graphql/create-group.mutation.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag';
 import MESSAGE_FRAGMENT from './message.fragment';
 
 const CREATE_GROUP_MUTATION = gql`
-  mutation createGroup($name: String!, $userIds: [Int!], $userId: Int!) {
-    createGroup(name: $name, userIds: $userIds, userId: $userId) {
+  mutation createGroup($name: String!, $userIds: [Int!]) {
+    createGroup(name: $name, userIds: $userIds) {
       id
       name
       users {
