0438e7cb75f9da7d7e1a242405bbdccf514ab02b
Simon Tucker <srtucker22@gmail.com>
2017-06-10 11:18:52 +0500

    Step 8.5: Add Input Types to Mutations

--- client/src/graphql/create-message.mutation.js
+++ client/src/graphql/create-message.mutation.js
@@ -3,8 +3,8 @@ import gql from 'graphql-tag';
 import MESSAGE_FRAGMENT from './message.fragment';
 
 const CREATE_MESSAGE_MUTATION = gql`
-  mutation createMessage($text: String!, $groupId: Int!) {
-    createMessage(text: $text, groupId: $groupId) {
+  mutation createMessage($message: CreateMessageInput!) {
+    createMessage(message: $message) {
       ... MessageFragment
     }
   }
