0e4d49eb99e50021b0932e6800eab38f40e5c8bc
Simon Tucker <srtucker22@gmail.com>
2017-06-10 10:31:07 +0500

    Step 8.2: Apply GROUP_FRAGMENT to Queries with default variables

--- client/src/graphql/create-group.mutation.js
+++ client/src/graphql/create-group.mutation.js
@@ -1,26 +1,14 @@
 import gql from 'graphql-tag';
 
-import MESSAGE_FRAGMENT from './message.fragment';
+import GROUP_FRAGMENT from './group.fragment';
 
 const CREATE_GROUP_MUTATION = gql`
-  mutation createGroup($name: String!, $userIds: [Int!]) {
+  mutation createGroup($name: String!, $userIds: [Int!], $first: Int = 1, $after: String, $last: Int, $before: String) {
     createGroup(name: $name, userIds: $userIds) {
-      id
-      name
-      users {
-        id
-      }
-      messages(first: 1) { # we don't need to use variables
-        edges {
-          cursor
-          node {
-            ... MessageFragment
-          }
-        }
-      }
+      ... GroupFragment
     }
   }
-  ${MESSAGE_FRAGMENT}
+  ${GROUP_FRAGMENT}
 `;
 
 export default CREATE_GROUP_MUTATION;
