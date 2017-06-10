7e7f5cd4e40786a98526b90418d9169a36b72972
Simon Tucker <srtucker22@gmail.com>
2017-06-10 11:18:52 +0500

    Step 8.5: Add Input Types to Mutations

--- client/src/graphql/group.query.js
+++ client/src/graphql/group.query.js
@@ -3,7 +3,7 @@ import gql from 'graphql-tag';
 import GROUP_FRAGMENT from './group.fragment';
 
 const GROUP_QUERY = gql`
-  query group($groupId: Int!, $first: Int = 1, $after: String, $last: Int, $before: String) {
+  query group($groupId: Int!, $messageConnection: ConnectionInput = {first: 0}) {
     group(id: $groupId) {
       ... GroupFragment
     }
