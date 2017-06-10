7d83d795f18130aa53ea9f0ca8c8c54a7b350288
Simon Tucker <srtucker22@gmail.com>
2017-06-10 10:31:07 +0500

    Step 8.2: Apply GROUP_FRAGMENT to Queries with default variables

--- client/src/graphql/group-added.subscription.js
+++ client/src/graphql/group-added.subscription.js
@@ -1,23 +1,14 @@
 import gql from 'graphql-tag';
 
-import MESSAGE_FRAGMENT from './message.fragment';
+import GROUP_FRAGMENT from './group.fragment';
 
 const GROUP_ADDED_SUBSCRIPTION = gql`
-  subscription onGroupAdded($userId: Int){
+  subscription onGroupAdded($userId: Int, $first: Int = 1, $after: String, $last: Int, $before: String){
     groupAdded(userId: $userId){
-      id
-      name
-      messages(first: 1) {
-        edges {
-          cursor
-          node {
-            ... MessageFragment
-          }
-        }
-      }
+      ... GroupFragment
     }
   }
-  ${MESSAGE_FRAGMENT}
+  ${GROUP_FRAGMENT}
 `;
 
 export default GROUP_ADDED_SUBSCRIPTION;
