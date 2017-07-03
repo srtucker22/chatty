20ccb2738c292517ea4d9df5d0a391d9ee11d605
Simon Tucker <srtucker22@gmail.com>
2017-07-03 09:17:35 -0700

    Step 6.1: Add Subscription to Schema

--- server/data/schema.js
+++ server/data/schema.js
@@ -67,10 +67,17 @@ export const Schema = [`
     leaveGroup(id: Int!, userId: Int!): Group # let user leave group
     updateGroup(id: Int!, name: String): Group
   }
+
+  type Subscription {
+    # Subscription fires on every message added
+    # for any of the groups with one of these groupIds
+    messageAdded(userId: Int, groupIds: [Int]): Message
+  }
   
   schema {
     query: Query
     mutation: Mutation
+    subscription: Subscription
   }
 `];
 
