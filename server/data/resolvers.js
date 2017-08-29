35bead484b8a58e640b9ebc3ca5d79d02106718f
Simon Tucker <srtucker22@gmail.com>
2017-08-29 14:55:42 -0700

    Step 5.2: Update Resolvers with Relay Cursor Connection

--- server/data/resolvers.js
+++ server/data/resolvers.js
@@ -1,9 +1,17 @@
 import GraphQLDate from 'graphql-date';
-
 import { Group, Message, User } from './connectors';
 
 export const Resolvers = {
   Date: GraphQLDate,
+  PageInfo: {
+    // we will have each connection supply its own hasNextPage/hasPreviousPage functions!
+    hasNextPage(connection, args) {
+      return connection.hasNextPage();
+    },
+    hasPreviousPage(connection, args) {
+      return connection.hasPreviousPage();
+    },
+  },
   Query: {
     group(_, args) {
       return Group.find({ where: args });
@@ -69,10 +77,62 @@ export const Resolvers = {
     users(group) {
       return group.getUsers();
     },
-    messages(group) {
+    messages(group, { first, last, before, after }) {
+      // base query -- get messages from the right group
+      const where = { groupId: group.id };
+
+      // because we return messages from newest -> oldest
+      // before actually means newer (id > cursor)
+      // after actually means older (id < cursor)
+
+      if (before) {
+        // convert base-64 to utf8 id
+        where.id = { $gt: Buffer.from(before, 'base64').toString() };
+      }
+
+      if (after) {
+        where.id = { $lt: Buffer.from(after, 'base64').toString() };
+      }
+
       return Message.findAll({
-        where: { groupId: group.id },
-        order: [['createdAt', 'DESC']],
+        where,
+        order: [['id', 'DESC']],
+        limit: first || last,
+      }).then((messages) => {
+        const edges = messages.map(message => ({
+          cursor: Buffer.from(message.id.toString()).toString('base64'), // convert id to cursor
+          node: message, // the node is the message itself
+        }));
+
+        return {
+          edges,
+          pageInfo: {
+            hasNextPage() {
+              if (messages.length < (last || first)) {
+                return Promise.resolve(false);
+              }
+
+              return Message.findOne({
+                where: {
+                  groupId: group.id,
+                  id: {
+                    [before ? '$gt' : '$lt']: messages[messages.length - 1].id,
+                  },
+                },
+                order: [['id', 'DESC']],
+              }).then(message => !!message);
+            },
+            hasPreviousPage() {
+              return Message.findOne({
+                where: {
+                  groupId: group.id,
+                  id: where.id,
+                },
+                order: [['id']],
+              }).then(message => !!message);
+            },
+          },
+        };
       });
     },
   },
